1ba58f50dda1573069ede9df093d1219b0f0e9a6
global <asgagagsad@gmail.com>
2022-07-12 18:58:18 +0300

    code review

--- string_sum.go
+++ string_sum.go
@@ -11,6 +11,10 @@ import (
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
+	
+	badInput = func(err error) error{
+		return fmt.Errorf("Input is wrong: %w", err)
+	}
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
@@ -24,11 +28,10 @@ func StringSum(input string) (output string, err error) {
 		return "", fmt.Errorf("Input went wrong: %w", errorEmptyInput)
 	}
 	b := 0
-	a := make([]int, 0, 2)
+	a := make([]int, 0)
 	for i := 0; i < len(input); i++ {
 
 		if i == 0 {
-
 			continue
 		}
 
@@ -36,22 +39,27 @@ func StringSum(input string) (output string, err error) {
 
 			value, err := strconv.Atoi(input[b:i])
 			if err != nil {
-				return "", fmt.Errorf("Input is wrong: %w", err)
+				return "", badInput(err)
 			}
+			
 			b = i
 			a = append(a, value)
+			
 		}
+		
 		if i == len(input)-1 {
+			
 			value, err := strconv.Atoi(input[b : i+1])
 			if err != nil {
-				return "", fmt.Errorf("Input is wrong: %w", err)
+				return "", badInput(err)
 			}
+			
 			a = append(a, value)
 		}
 
 	}
 	if len(a) != 2 {
-		return "", fmt.Errorf("Input is wrong: %w", errorNotTwoOperands)
+		return "", badInput(err)
 	}
 	output = strconv.Itoa(a[0] + a[1]) 
 	return output, nil
